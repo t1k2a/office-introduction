10d1883df64cf5c6b83abdca08c1329e4a461fc6
t1k2a <maxtj1122@gmail.com>
2019-04-17 00:37:19 +0000

    finished commit

---
+++ vr_index.php
@@ -0,0 +1,13 @@
+<html><body>
+<?php
+
+readfile("https://vr-n2.glitch.me/");
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+
+?>
+</body>
+</html>
